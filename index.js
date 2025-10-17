4845d1dba8dc1a3351271d58095bd7d32c43eaad
Brendon W <bwherry@midland.k12.ia.us>
2025-10-17 16:58:33 +0000

    gg

--- index.js
+++ index.js
@@ -10,4 +10,5 @@ let newJobPost = `Dear ${firstName},
 Thank you for deciding to join us! 
 Youll be working as our new ${jobTitle} here in ${city}`
 
+
 document.getElementById("result").innerHTML = newJobPost;
